00ff1df27735e881de41bbe5e01276054222edc2
rafaelangical <rsnrafael@outlook.com>
2018-04-23 21:53:40 -0300

    New icons of likedin and github

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Hero.css';
-import perfil from './perfil.jpg';
+import perfil from './perfill.jpg';
+import likedin from './likedin.png';
+import github from './github.png';
 import Navegation from '../Navegation.js';
 
 export default class Hero extends Component {
@@ -13,10 +15,19 @@ export default class Hero extends Component {
             <img src={perfil} className="hero-image" alt="Rafael Souza"/>
           </div>
           <div className="hero-content-bottom">
-            <h1>Rafael Souza</h1>
-            <hr />
-            <p className="p-hero"><code>I am a web developer with a focus on front-end
-            <br/> developing with new and secure technologies.</code></p>
+            <div className="hero-content-bottom-top">
+              <h1>Rafael Souza</h1>
+              <hr />
+              <p className="p-hero"><code>I am a web developer with a focus on front-end
+              <br />developing with new and secure technologies.</code></p>
+            </div>
+            <div className="hero-content-bottom-bottom">
+              <a href="http://www.linkedin.com/in/rafaelangical" target="_blank">
+                <img src={likedin} href="#" alt="likedin" className="images-socials" />
+              </a>              
+              
+              <a href="https://github.com/rafaelangical" target="_blank"> <img src={github} alt="github" className="images-socials" /> </a>
+            </div>
           </div>
         </div>
       </div>
