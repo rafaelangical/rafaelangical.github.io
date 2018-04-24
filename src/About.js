66a43e50c5cd4a8344dc4c4972a44fc7f9bf2a74
rafaelangical <rsnrafael@outlook.com>
2018-04-23 21:10:37 -0300

    Updated the h3

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ export default class About extends Component{
 			<div className="Container-internal container-about">
 				<Navegation />
 				<div className="div-about">
-					<h3 className="h3-about">About</h3>
+					<h3 className="h3-about">Hello :)</h3>
 					<p className="p-about-p p-about-p-small">I am an eternal passionate about technologies and challenges, I am currently developing "web links to improve things and make our daily lives easier haha", I develop exclusively with Javascript (Language of the future) with React and React Native I am by gradually migrating to FullStack Js (In the future).</p>
 				</div>
 			</div>	
