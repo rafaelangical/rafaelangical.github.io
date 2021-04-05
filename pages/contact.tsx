307ea7f342c27aa076c13fa8f6ff5c57a729e185
Rafael Souza <rafael.nascimento-ext@viavarejo.com.br>
2021-04-05 09:14:08 -0300

    Add contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,23 @@
+import Button from 'components/button/button';
 import Layout from '../components/layout/layout';
+import { Fieldset, Form, Input, Label } from './styles/contact';
 
 const Contact: React.FC = () => {
   return (
     <Layout title="Contact">
-      <h1>Contact</h1>
+      <Form>
+        <p>Hello, it will be a pleasure to you, fill in the fields below and as soon as possible I will return <span role="img" aria-label="emoji">😉</span>  Thank you.</p>
+        <Label htmlFor="subject">Subject</Label>
+        <Input type="text" name="subject" />
+        <Label htmlFor="email">E-mail</Label>
+        <Input type="email" name="email" />
+        <Label htmlFor="phone">Phone</Label>
+        <Input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" />
+        <Label htmlFor="message">Message</Label>
+        <Fieldset name="message" />
+
+        <Button title="Submit" onClick={() => alert("clique")} />
+      </Form>
     </Layout>
   )
 };
---
+++ pages/styles/contact.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  width: 100%;
+  height: auto;
+  max-width: 992px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+
+  h1, p {
+    margin: 0;
+    padding: 0;
+  }
+  p {
+    margin: 20px 0;
+    font-size: 2em;
+  }
+
+  @media (max-width: 768px) { 
+    p {
+      font-size: 18px !important;
+    }
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 36px;
+  font-size: 18px;
+  border: 2px solid darkblue;
+  border-radius: 4px;
+  color: #000;
+  padding: 0px 10px;
+  margin: 16px 0px;
+  
+  @media (max-width: 768px) { 
+    font-size: 14px;
+  }
+`;
+
+export const Fieldset = styled.textarea`
+  width: 100%;
+  height: 120px;
+  font-size: 18px;
+  border: 2px solid darkblue;
+  border-radius: 4px;
+  color: #000;
+  padding: 0px 10px;
+  margin: 16px 0px;
+
+  @media (max-width: 768px) { 
+    font-size: 14px;
+  }
+`;
+
+
+export const Label = styled.label`
+  font-size: 18px;
+  color: darkblue;
+  font-weight: bold;
+  @media (max-width: 768px) { 
+    font-size: 14px;
+  }
+`;
