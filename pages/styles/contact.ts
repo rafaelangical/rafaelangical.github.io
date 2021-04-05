b4b077070e96f544171eb73aed7458bd1ba34538
Rafael Souza <rafael.nascimento-ext@viavarejo.com.br>
2021-04-05 09:32:57 -0300

    Clean up on styles of page contact

--- pages/styles/contact.ts
+++ pages/styles/contact.ts
@@ -24,22 +24,6 @@ export const Form = styled.form`
   }
 `;
 
-export const Fieldset = styled.textarea`
-  width: 100%;
-  height: 120px;
-  font-size: 18px;
-  border: 2px solid darkblue;
-  border-radius: 4px;
-  color: #000;
-  padding: 0px 10px;
-  margin: 16px 0px;
-
-  @media (max-width: 768px) { 
-    font-size: 14px;
-  }
-`;
-
-
 export const Label = styled.label`
   font-size: 18px;
   color: darkblue;
